de554858faa18f3c09a5c8dae0b4f89d64ac188b
MuhammadAsghar1999 <143824430+MuhammadAsghar1999@users.noreply.github.com>
2023-10-06 00:17:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,106 @@
+// chapter function
+// Q:1
+// function displayCurrentDateTime() {
+//     var currentDate = new Date();
+//     var datetimeString = currentDate.toLocaleString()
+//     console.log("Current Date & Time: " + datetimeString);
+// }
+// displayCurrentDateTime();
+
+// Q:2
+
+// function greet(){
+//     var Firstname = prompt("Enter First Name");
+//     var Lastname = prompt ("Enter last Name");
+//     var Fullname = Firstname + Lastname;
+//     console.log(Fullname)
+// }
+// greet();
+
+// Q:3
+
+// function sum(){
+//     console.log(user + user1)
+// }
+// var user =+prompt("Enter number")
+// var user1 =+prompt("Enter sumnumber")
+// sum(10,20)
+// sum();
+
+// Q:4
+
+// function calculator(Num1,opr,Num2){
+// if(opr === "+"){
+//     return Num1 + Num2;
+// }else if(opr ==="-"){
+//     return Num1 = Num2;
+// }else if(opr === "*"){
+//     return Num1 * Num2;
+// }else if(opr === "/"){
+//     return Num1 / Num2;
+// }
+// }
+// var Num1 = +prompt("Enter First Value")
+// var opr = +prompt("Enter operator Value")
+// var Num2 = +prompt("Enter Second Value")
+// var result = calculator(Num1 + opr + Num2)
+// document.write(result)
+// calculator(Num1,opr,Num2);
+
+// Q:5
+
+// function square(number) {
+//     return number * number;
+// }
+// const result = square(5);
+// console.log(result);
+
+// Q:6
+
+// function factorialRecursive(number) {
+//     if (number === 0) {
+//         return 1;
+//     }
+//     else {
+//         return number * factorialRecursive(number - 1);
+//     }
+// }
+// const result = factorialRecursive(5); 
+// console.log(result); 
+
+// Q:7
+
+// function displayCounting(start, end) {
+//     if (start <= end) {
+//         for (let i = start; i <= end; i++) {
+//             console.log(i);
+//         }
+//     } else {
+//         for (let i = start; i >= end; i--) {
+//             console.log(i);
+//         }
+//     }
+// }
+// console.log("Counting from 1 to 5:");
+// displayCounting(1, 5);
+
+// console.log("Counting from 10 to 1:");
+// displayCounting(10, 1);
+
+// console.log("Counting from -3 to 3:");
+// displayCounting(-3, 3);
+
+// Q:8
+
+// Q:9
+
+// function calculateRectangleArea(width, height) {
+//     return width * height;
+// }
+// const area1 = calculateRectangleArea(5, 6);
+// console.log(`Area rectangle: ${area1}`);
+
+// Q:10
+
+// Q:11
+
